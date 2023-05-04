48ca12360a85f6b1ab08a13ef1f8574a8bccb205
JUNG BOBAE <acarid27@gmail.com>
2023-05-04 20:28:27 +0900

    초기 커밋.

---
+++ TextRPG_Practice/Game.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG_Practice
+{
+    internal class Game
+    {
+        private bool running = true;
+
+        public void Run()
+        {
+            // Init
+            
+            // Loop
+            while (running)
+            {
+                // Input
+                // Update
+                // Render
+            }
+            // Release
+
+        }
+    }
+}
